42941a16656ee8e7f02dfe9742671bac536bfb53
MosesChisz <mosesc@cortexware.co.za>
2024-05-15 09:13:43 +0000

    server.js

--- server.js
+++ server.js
@@ -1,28 +1,14 @@
 import express from 'express';
-import router from './routes/index';
-import unmatchedRouteHandler from './middleware/unmatched';
-import errorHandler from './middleware/error';
-import shutdown from './utils/shutdown';
+import routes from './routes';
 
-// Express server
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// parse JSON request bodies
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(router);
-app.use(unmatchedRouteHandler);
-app.use(errorHandler);
+// load routes
+app.use('/', routes);
 
-const server = app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-// Graceful shutdown
-const handler = () => shutdown(server);
-process.on('SIGINT', handler);
-process.on('SIGTERM', handler);
-process.on('SIGQUIT', handler);
-
-export default app;
