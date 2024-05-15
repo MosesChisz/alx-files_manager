ebbb383442b26114bf3db543ce1424bc318779de
MosesChisz <mosesc@cortexware.co.za>
2024-05-15 09:00:32 +0000

    AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,37 +1,27 @@
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
-class AppController {
-  /**
-   * Controller for endpoint GET /status that retrieves
-   * mongodb client and redis client connection status
-   * @typedef {import("express").Request} Request
-   * @typedef {import("express").Response} Response
-   * @param {Request} _req - request object
-   * @param {Response} res  - response object
-   */
-  static getStatus(_req, res) {
-    if (dbClient.isAlive() && redisClient.isAlive()) {
-      res.status(200).json({ redis: true, db: true });
-    }
-  }
+const AppController = {
+  async getStatus(req, res) {
+    const redisStatus = redisClient.isAlive();
+    const dbStatus = dbClient.isAlive();
 
-  /**
-   * Controller for endpoint GET /stats that retrieves
-   * count of users and files
-   * @param {Request} _req - Request object
-   * @param {Response} res  - Response object
-   * @param {import("express").NextFunction} next - Next function
-   */
-  static async getStats(_req, res, next) {
-    try {
-      const users = await dbClient.nbUsers();
-      const files = await dbClient.nbFiles();
-      res.status(200).json({ users, files });
-    } catch (err) {
-      next(err);
+    if (redisStatus && dbStatus) {
+      res.status(200).json({ redis: true, db: true });
+    } else {
+      res.status(500).json({ redis: redisStatus, db: dbStatus });
     }
-  }
-}
+  },
+
+  async getStats(req, res) {
+    try {
+      const usersCount = await dbClient.nbUsers();
+      const filesCount = await dbClient.nbFiles();
+      res.status(200).json({ users: usersCount, files: filesCount });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+};
 
 export default AppController;
